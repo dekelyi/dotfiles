42e1cbe17ce36140b9804f99c8f5548f88dbfe19
Dekel Yigal <dekelyi@gmail.com>
2020-11-14 15:09:14 +0200

    nvim: fix theme.lua

--- .config/nvim/lua/theme.lua
+++ .config/nvim/lua/theme.lua
@@ -1,28 +1,58 @@
-require'nvim-treesitter.configs'.setup {
+require('nvim-treesitter.configs').setup {
 	ensure_installed = "all",     -- one of "all", "language", or a list of languages
 	highlight = {
 		enable = true,              -- false will disable the whole extension
 	},
 }
 
-vim.g.lightline = {
-	-- 'component_function' = {
-	-- 	'mode' = 'LightlineMode',
-	-- 	'fileformat' = 'LightlineFileformat',
-	-- 	'filetype' = 'LightlineFiletype',
-	-- },
+local lightline = {
+	-- colorscheme = 'onedark',
+	component_function = {
+		mode = "LightlineMode",
+		fileformat = "LightlineFileformat",
+		filetype = "LightlineFiletype",
+	},
 }
 
+function ThemeSwitch(color)
+	if color == nil then
+		if vim.o.background == 'dark' then
+			color = 'light'
+		else
+			color = 'dark'
+		end
+	end
 
-function ThemeSwitch()
 	theme = ''
-	if vim.o.background == 'dark' then
+
+	if color == 'light' then
 		theme = 'PaperColor'
-	vim.o.background = 'light'
+		vim.o.background = 'light'
 	else
 		theme = 'onedark'
-	vim.o.background = 'dark'
+		vim.o.background = 'dark'
 	end
-	vim.g.lightline.colorscheme = theme
+
+	lightline.colorscheme = theme
 	vim.cmd(string.format('colorscheme %s', theme))
+	RedrawStatus()
 end
+
+function RedrawStatus()
+	vim.g.lightline = lightline
+	vim.fn["lightline#init"]()
+	vim.fn["lightline#colorscheme"]()
+	vim.fn["lightline#update"]()
+end
+
+vim.o.termguicolors = true
+vim.o.showmode = false
+vim.o.inccommand = 'nosplit'
+vim.o.hlsearch = true
+vim.o.incsearch = true
+
+vim.g.indentLine_leadingSpaceEnabled = true
+
+vim.cmd [[command! ThemeSwitch lua ThemeSwitch()]]
+ThemeSwitch('dark')
+
--- .config/nvim/plugin/theme.vim
+++ .config/nvim/plugin/theme.vim
@@ -1,45 +1,9 @@
 lua require('theme')
 
-let g:indentLine_leadingSpaceEnabled = 1
-set noshowmode
-set inccommand=nosplit
-set hlsearch incsearch
-
-let g:lightline = {}
-
-" ################### Colorscheme ############
-
-function! ThemeDark()
-	let g:lightline.colorscheme='onedark'
-	set background=dark
-	colorscheme onedark
-endfunction
-
-function! ThemeLight()
-	set background=light
-	colorscheme PaperColor
-	let g:lightline.colorscheme='PaperColor'
-endfunction
-
-function! ThemeSwitch()
-	if &background == 'dark'
-		call ThemeLight()
-	else
-		call ThemeDark()
-	endif
-	call RedrawLightline()
-endfunction
-
-command! ThemeSwitch call ThemeSwitch()
-
-if !has('gui_running')
-	set termguicolors
-elseif
-	set t_Co=256
-endif
-call ThemeDark()
-
-" ################## Lightline ##################
+" let g:indentLine_leadingSpaceEnabled = 1
+" set noshowmode
+" set inccommand=nosplit
+" set hlsearch incsearch
 
 function! LightlineMode()
 	return  &filetype ==# 'CHADTree' ? 'CHADTree' : lightline#mode()
@@ -53,18 +17,4 @@ function! LightlineFiletype()
 	return winwidth(0) > 70 ? (&filetype !=# '' ? &filetype : 'no ft') : ''
 endfunction
 
-function! RedrawLightline()
-	let g:lightline = {
-				\ 		'colorscheme': g:lightline.colorscheme,
-				\ 		'component_function': {
-				\   		'mode': 'LightlineMode',
-				\		   	'fileformat': 'LightlineFileformat',
-				\		   	'filetype': 'LightlineFiletype',
-				\ 		},
-				\ 	}
-
-	call lightline#init()
-	call lightline#colorscheme()
-	call lightline#update()
-endfunction
-call RedrawLightline()
+" lua ThemeSwitch()
