689eb438d4da59a5ad149c8d9b22212d89267a58
Dekel Yigal <dekelyi@gmail.com>
2020-11-15 01:57:28 +0200

    refactor(nvim): convert lightline components to lua
    		also refactor `theme.lua` to be a module

--- .config/nvim/lua/theme.lua
+++ .config/nvim/lua/theme.lua
@@ -1,20 +1,35 @@
-require('nvim-treesitter.configs').setup {
-	ensure_installed = "all",     -- one of "all", "language", or a list of languages
-	highlight = {
-		enable = true,              -- false will disable the whole extension
-	},
+local M = {}
+
+------ Lightline
+local function _widthlimit(str)
+	if vim.api.nvim_win_get_width(0) > 70 then
+		return str
+	else
+		return ''
+	end
+end
+
+M.lightline_components = {
+	fileformat = function() _widthlimit(vim.o.fileformat) end,
+	filetype = function() _widthlimit(vim.o.filetype) end
 }
 
-local lightline = {
-	-- colorscheme = 'onedark',
-	component_function = {
-		mode = "LightlineMode",
-		fileformat = "LightlineFileformat",
-		filetype = "LightlineFiletype",
-	},
-}
+local lightline_config = { component_function = {} }
+for key,val in pairs(M.lightline_components) do
+	lightline_config.component_function[key] = string.format("v:lua.require('theme').lightline_components.%s()", key)
+end
 
-function ThemeSwitch(color)
+function M.RedrawStatus()
+	vim.g.lightline = lightline_config
+	vim.fn["lightline#init"]()
+	vim.fn["lightline#colorscheme"]()
+	vim.fn["lightline#update"]()
+end
+
+------ Theme
+function M.ThemeSwitch(...)
+	local color = select(1, ...)
+
 	if color == nil then
 		if vim.o.background == 'dark' then
 			color = 'light'
@@ -33,18 +48,12 @@ function ThemeSwitch(color)
 		vim.o.background = 'dark'
 	end
 
-	lightline.colorscheme = theme
+	lightline_config.colorscheme = theme
 	vim.cmd(string.format('colorscheme %s', theme))
-	RedrawStatus()
+	M.RedrawStatus()
 end
 
-function RedrawStatus()
-	vim.g.lightline = lightline
-	vim.fn["lightline#init"]()
-	vim.fn["lightline#colorscheme"]()
-	vim.fn["lightline#update"]()
-end
-
+------ Options
 vim.o.termguicolors = true
 vim.o.showmode = false
 vim.o.inccommand = 'nosplit'
@@ -53,6 +62,17 @@ vim.o.incsearch = true
 
 vim.g.indentLine_leadingSpaceEnabled = true
 
-vim.cmd [[command! ThemeSwitch lua ThemeSwitch()]]
-ThemeSwitch('dark')
+------ Commands
+vimp.map_command('ThemeSwitch', M.ThemeSwitch)
 
+------ Setup
+require('nvim-treesitter.configs').setup {
+	ensure_installed = "all",     -- one of "all", "language", or a list of languages
+	highlight = {
+		enable = true,              -- false will disable the whole extension
+	},
+}
+
+M.ThemeSwitch('dark')
+
+return M
